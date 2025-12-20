e11e19431688e1b09e74bcf0b8fc35928451c1dd
YanRui06 <yanrui060127@outlook.com>
2025-12-20 23:07:29 +0800

    更新配置文件中的基础URL地址

--- Agent/local.py
+++ Agent/local.py
@@ -4,7 +4,7 @@ import websocket
 
 if __name__ == '__main__':
     chatbot = Advent()
-    wb = websocket.create_connection("ws://192.168.31.100:5788/")
+    wb = websocket.create_connection("ws://8.148.5.68:5788/")
     while True:
         user_input = input("请输入：")
         if user_input == "exit":
